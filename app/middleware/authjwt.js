7b7b5e45aa9eff042253c39e1b8b0254c28b5862
Corey Johnson <corey.johnson@gofore.com>
2024-05-20 17:07:15 +0300

    Converted callback to promise on authjwt.js, as there was network error when admin tried to view admin content.

--- app/middleware/authjwt.js
+++ app/middleware/authjwt.js
@@ -25,65 +25,60 @@ verifyToken = (req, res, next) => {
 };
 
 isAdmin = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-
-    Role.find(
-      {
-        _id: { $in: user.roles }
-      },
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-
+  if (req.userId) {
+  User.findById(req.userId)
+  .then(user => {
+    Role.find({_id: { $in: user.roles }})
+    .then(roles => {
         for (let i = 0; i < roles.length; i++) {
           if (roles[i].name === "admin") {
             next();
             return;
           }
         }
-
-        res.status(403).send({ message: "Require Admin Role!" });
-        return;
-      }
-    );
+        // If the loop completes without finding an admin role
+        res.status(403).send({ message: "Admin access required!" });
+      })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ message: err });
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).send({ message: err });
   });
+  } else {
+    res.status(403).send({ message: "No user ID provided!" });
+  }
 };
 
 isModerator = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-
-    Role.find(
-      {
-        _id: { $in: user.roles }
-      },
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-
+  if (req.userId) {
+    User.findById(req.userId)
+    .then(user => {
+      Role.find({_id: { $in: user.roles }})
+      .then(roles => {
         for (let i = 0; i < roles.length; i++) {
           if (roles[i].name === "moderator") {
             next();
             return;
           }
         }
-
-        res.status(403).send({ message: "Require Moderator Role!" });
-        return;
-      }
-    );
-  });
+        res.status(403).send({ message: "Moderator Rights Required!" });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send({ message: err });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ message: err });
+    });
+  } else {
+    res.status(403).send({ message: "No user ID provided!" });
+  }
 };
 
 const authJwt = {
